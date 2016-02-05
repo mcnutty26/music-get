8072c50210d8dfd40c427b40e0daff8786e13754
mcnutty26 <williamseymour123@googlemail.com>
2016-02-05 18:46:59 +0000

    domain fix

--- src/mcnutty/music/get/ProcessServer.java
+++ src/mcnutty/music/get/ProcessServer.java
@@ -207,7 +207,7 @@ public class ProcessServer extends AbstractHandler {
     
     //redirect the requester back to the front end
     void redirect(HttpServletRequest request, HttpServletResponse response, String page) {
-    	String name = "music";
+    	String name = "music.lan";
     	String url = "http://" + name + "/" + page + ".php";
     	response.setContentLength(0);
     	try {
