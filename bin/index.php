04dd851105370f3bb94dc400934268273672bc52
mcnutty26 <williamseymour123@googlemail.com>
2016-02-06 18:21:19 +0000

    fixed aliasing

--- bin/index.php
+++ bin/index.php
@@ -104,7 +104,7 @@
               <td></td>
               <td></td>
             </tr>
-        <?php
+<?php
             $json = file_get_contents("http://localhost:8080/list");
             $data = json_decode($json, true);
             $json_last = file_get_contents("http://localhost:8080/last");
@@ -115,7 +115,7 @@
             $last = array();
 
             foreach ($data as $item) {
-                array_push($queue, array($item['name'], $item['ip'], $item['guid'], $item['alias']));
+		array_push($queue, array($item['name'], $item['ip'], $item['guid'], $item['alias']));
             }
             foreach ($data_last as $item) {
                 array_push($last, $item['ip']);
@@ -140,7 +140,7 @@
                 foreach ($bucket as $item) {
                     echo "<tr>";
                     echo "<td>" . substr($item[0], 0, 70) . "</td>";
-                    echo "<td>" . ($item[3] != "" ? substr($item[3], 0, 20) : $item[1]) . "</td>";
+		    echo "<td>" . ($item[3] != "" ? substr($item[3], 0, 20) : $item[1]) . "</td>";
                     $guid = $item[2];
                     echo "<td>" . ($client_ip == $item[1] ? "<a class=\"fui-cross ajax-button\" onclick=\"remove_item('$guid')\"></a>" : "") . "</td>";
                     echo "</tr>";
@@ -167,7 +167,7 @@
 	</form>
 	</div>
 
-	<?php if (file_get_contents("http://localhost:8080/alias") == "canalias") {?>
+	<?php if (file_get_contents("http://localhost:8080/alias?ip=" . rawurlencode($client_ip)) == "canalias") {?>
 	<div class="form-row">
 	    <h6>Set an alias for yourself (once per LAN)</h6>
 	    <form action="http://<?=$server_name?>:8080/alias/add" method="post" enctype="multipart/form-data">
